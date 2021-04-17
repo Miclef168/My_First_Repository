d01618333b17c686dd6f888b51dd766b123da073
Miclef168 <franklin.micle@gmail.com>
2021-04-17 12:17:11 -0400

    First day of java
    Activities from 19.1

---
+++ 19-Week/19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,16 @@
+// Create the addNumbers function here.
+function addNumbers() {
+    var firstNumber=2;
+    var secondNumber=3;
+    var firstSum= firstNumber + secondNumber;
+    console.log(firstSum);
+}
+addNumbers();
+// Create the stringConcat function here.
+function stringConcat () {
+    var firstFood='taco';
+    var firstAnimal='cat';
+    var wordCombo=firstFood + ' ' + firstAnimal;
+    alert(wordCombo);
+}
+stringConcat();
